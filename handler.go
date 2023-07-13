84e7dac7763455873067ea30b8906753e5a8333f
Begonia <2239849100@qq.com>
2023-07-13 17:41:22 +0800

    add: use map and replace stub

--- handler.go
+++ handler.go
@@ -10,18 +10,31 @@ import (
 // StudentServiceImpl implements the last service interface defined in the IDL.
 type StudentServiceImpl struct{}
 
+var studentmap map[int]demo.Student = make(map[int]demo.Student)
+
 // Register implements the StudentServiceImpl interface.
 func (s *StudentServiceImpl) Register(ctx context.Context, student *demo.Student) (resp *demo.RegisterResp, err error) {
 	// TODO: Your code here...
 
+	_, ok := studentmap[int(student.Id)]
+
+	if ok {
+		resp = &demo.RegisterResp{
+			Message: "id exist!",
+			Success: false,
+		}
+		return
+	}
+
+	studentmap[int(student.Id)] = *student
 
 	//stub
-	klog.Infof("Received: ID: %d, Name: %s",student.Id,student.Name )
+	klog.Infof("Received: ID: %d, Name: %s", student.Id, student.Name)
 	resp = &demo.RegisterResp{
 		Message: "OK!",
 		Success: true,
 	}
-	
+
 	return
 }
 
@@ -29,18 +42,26 @@ func (s *StudentServiceImpl) Register(ctx context.Context, student *demo.Student
 func (s *StudentServiceImpl) Query(ctx context.Context, req *demo.QueryReq) (resp *demo.Student, err error) {
 	// TODO: Your code here...
 
+	value, ok := studentmap[int(req.Id)]
+	if !ok {
+		resp = nil
+		return
+	}
+
+	resp = &value
 
 	//stub
-	klog.Infof("Your query: %d",req.Id)
-
-	resp = &demo.Student{
-		Id: 10,
-		Name: "begonia",
-		College: &demo.College{
-			Name: "NJU",
-			Address: "no",
-		},
-		Email: nil,
-	}
-	return 
+	klog.Infof("query: %d", req.Id)
+	/*
+		resp = &demo.Student{
+			Id: 10,
+			Name: "begonia",
+			College: &demo.College{
+				Name: "NJU",
+				Address: "no",
+			},
+			Email: nil,
+		}
+	*/
+	return
 }
