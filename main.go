179c6a0a77337c88e7b4f23a28e4178bc2c956a0
Begonia <2239849100@qq.com>
2023-07-13 10:15:03 +0800

    nothing important

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"log"
 	"net"
 	"project3/kitex_gen/kitex/demo/studentservice"
-	demo "project3/kitex_gen/kitex/demo/studentservice"
+	// demo "project3/kitex_gen/kitex/demo/studentservice"
 	"github.com/cloudwego/kitex/server"
 )
 
 func main() {
 	// svr := demo.NewServer(new(StudentServiceImpl))
 	var svr server.Server
+	
 	
 	addr, _ := net.ResolveTCPAddr("tcp", ":9999")
 	svr = studentservice.NewServer(new(StudentServiceImpl), server.WithServiceAddr(addr))
